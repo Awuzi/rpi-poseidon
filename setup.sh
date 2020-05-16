46d81df1710f954abe48995b42adad0d3d85219f
stan-racz <stan.racz7@gmail.com>
2020-05-16 21:13:41 +0200

    ajout d'un prototype de script d'installation

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#install script to get machine ready
+apt-get update  # To get the latest package lists
+apt-get install python3-pip
+apt-get install sqlite3
+apt-get install python3
+apt-get install
+pip3 install paho-mqtt
+pip3 install sense_emu
+pip3 install sense_hat
+pip3 install pygal
+pip3 install flask
+pip3 install sqlite3
+pip3 install Logging.handlers
